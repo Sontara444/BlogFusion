d3c0f34226dffe876e9f8dbb0d08193157640226
Sontara444 <sontararajput@gmail.com>
2024-07-12 22:30:23 +0530

    setup

--- routes/user.js
+++ routes/user.js
@@ -11,6 +11,13 @@ router.get('/signup', (req, res)=>{
     return res.render("signup")
 });
 
+router.post("/signin", async (req, res) =>{
+     const {email, password} = req.body;
+     console.log(email, password)
+      const token = await User.matchPasswordAndGenerateToken(email, password)
+      return res.cookie("token", token ).redirect("/");
+})
+
 router.post("/signup", async (req, res)=>{
     const {fullName, email, password } = req.body;
     await User.create({
